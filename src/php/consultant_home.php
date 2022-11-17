7c22b4ec3f86f2e6cd937903787365cd67a81af9
thilini.yashodha.demel <thilini.yashodha.demel@gmail.com>
2022-11-17 15:53:22 +0530

    New

--- src/php/consultant_home.php
+++ src/php/consultant_home.php
@@ -9,9 +9,13 @@ if (isset($_SESSION['user_name']) && isset($_SESSION['consultant_name']))
   <head>
     <meta charset="utf-8">
     <title>Consultant Home Page</title>
-    <link rel="stylesheet" type="text/css" href="../../public/css/consultant_home.css?v=2">
+    <link rel="stylesheet" type="text/css" href="../../public/css/consultant_home.css?v=1">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <script type="text/javascript" src="../../public/js/consultant_disease.js">
+
+    </script>
   </head>
   <body>
     <div class="hero">
@@ -42,6 +46,7 @@ if (isset($_SESSION['user_name']) && isset($_SESSION['consultant_name']))
         <div class="right">
           <div class="rtop">
             <div class="empty">
+              <p>Sri Lanakan Bone Marrow Transplant Management System</p>
             </div>
             <div class="profile">
               <abbr title="notifications"><a href="../../public/html/consultant_notifications.html">
@@ -71,6 +76,121 @@ if (isset($_SESSION['user_name']) && isset($_SESSION['consultant_name']))
             </div>
           </div>
           <div class="rbottom">
+            <div class="main">
+              <div class="banner">
+                <div class="phara">
+                  <p>This is a phara. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </p>
+                  <button type="button" name="button">Read More</button>
+                </div>
+              </div>
+              <div class="cases">
+                <a href="">
+                <div class="case">
+                  <div class="name">
+                    <span class="material-symbols-rounded">
+                      healing
+                    </span>
+                    Primary Care
+                  </div>
+                  <div class="descript">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  </div>
+                </div>
+                </a>
+                <a href="">
+                <div class="case">
+                  <div class="name">
+                    <span class="material-symbols-rounded">
+                      emergency
+                    </span>
+                    Emergency Cases
+                  </div>
+                  <div class="descript">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  </div>
+                </div>
+                </a>
+                <a href="">
+                <div class="case">
+                  <div class="name">
+                    <span class="material-symbols-rounded">
+                      calendar_apps_script
+                    </span>
+                    Online Appoinments
+                  </div>
+                  <div class="descript">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  </div>
+                  </a>
+                </div>
+              </div>
+              <div class="banner2">
+                <div class="phara2">
+                  <p>This is a phara. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </p>
+                  <button type="button" name="button">Read More</button>
+                </div>
+              </div>
+              <div class="read-m">
+                <div class="read">
+                  <div class="pic">
+                    <span class="material-symbols-outlined">
+                      sick
+                    </span>
+                  </div>
+                  <div class="topic">
+                    Disease-Specific Indications and Outcomes
+                  </div>
+                  <div class="desc">
+                    <br>Find the latest references, outcomes, and referral timing guidelines by disease. Access patient-friendly handouts.
+                    <br>
+                  </div>
+                  <div class="but1">
+                    <a href="consultant_disease.php">Browse All Diseases</a><br>
+                    <select id="d1" name="disease" onchange="selectDisease();">
+                      <option selected="selected" disabled=disabled>Select the Disease</option>
+                      <option value="AML-Adult">AML - Adult</option>
+                      <option value="AML-Pediatric">AML-Pediatric</option>
+                      <option value="ALL-Adult">ALL-Adult</option>
+                      <option value="ALL-Pediatric">ALL-Pediatric</option>
+                      <option value="MDS">MDS</option>
+                      <option value="CML">CML</option>
+                      <option value="CLL">CLL</option>
+                      <option value="NHL">NHL</option>
+                      <option value="HodgkinLymphoma">Hodgkin Lymphoma</option>
+                      <option value="MultipleMyeloma">Multiple Myeloma</option>
+                      <option value="SevereAplasticAnemia&MarrowFailure">Severe Aplastic Anemia & Marrow Failure</option>
+                      <option value="SickleCellDisease">Sickle Cell Disease</option>
+                      <option value="ImmuneDeficiencyDiseases">Immune Deficiency Diseases</option>
+                      <option value="InheritedMetabolicDisorders">Inherited Metabolic Disorders</option>
+                      <option value="Thalassemia">Thalassemia</option>
+                      <option value="OtherDiseases">Other Diseases</option>
+                      <option value="MultipleSclerosis">Multiple Sclerosis</option>
+                      <option value="SystemicSclerosis">Systemic Sclerosis</option>
+                    </select>
+                    <!--<select class="fancy_select fancy_selects-active" name="diseases" id="diseases" onchange="gotoDisease(this.value);" style="opacity: 0; min-height: 100%; min-width: 100%; position: absolute; top: 0px; left: 0px; z-index: 2; cursor: pointer;">-->
+                  </div>
+                </div>
+                <div class="read">
+                  <div class="pic">
+                    <span class="material-symbols-outlined">
+                      rheumatology
+                    </span>
+                  </div>
+                  <div class="topic">
+                    HLA Today
+                  </div>
+                  <div class="desc">
+                      Remove barriers to patient and family member HLA typing at diagnosis. Get the critical info you need to make treatment decisions.
+                  </div>
+                  <div class="but2">
+                    <a href="consultant_discover.php">Discover How</a>
+                  </div>
+                </div>
+              </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
