ab73a2c87c983fb976a9c15b44af53ebee6146af
thilini.yashodha.demel <thilini.yashodha.demel@gmail.com>
2022-11-18 17:05:39 +0530

    New

--- src/php/consultant_calendar.php
+++ src/php/consultant_calendar.php
@@ -8,8 +8,8 @@ if (isset($_SESSION['user_name']) && isset($_SESSION['consultant_name']))
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Consultant Appointments</title>
-    <link rel="stylesheet" type="text/css" href="../../public/css/consultant_appointments.css?v=1">
+    <title>Consultant Calendar</title>
+    <link rel="stylesheet" type="text/css" href="../../public/css/consultant_calendar.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
   </head>
@@ -21,9 +21,9 @@ if (isset($_SESSION['user_name']) && isset($_SESSION['consultant_name']))
 
           <div class="tabs">
 
-            <a class="hi-selected">Appointments</a>
+            <a class="hi" href="consultant_appointments.php">Appointments</a>
             <a class="hi" href="consultant_reports.php">Reports</a>
-            <a class="hi" href="consultant_calendar.php">Calendar</a>
+            <a class="hi-selected">Calendar</a>
             <a class="hi" href="consultant_patient.php">Patient</a>
             <a class="hi" href="consultant_donor.php">Donor</a>
 
@@ -42,7 +42,7 @@ if (isset($_SESSION['user_name']) && isset($_SESSION['consultant_name']))
         <div class="right">
           <div class="rtop">
             <div class="empty">
-              <p>Appointments</p>
+              <p>Calendar</p>
             </div>
             <div class="profile">
               <abbr title="notifications"><a href="../../public/html/consultant_notifications.html">
@@ -79,61 +79,52 @@ if (isset($_SESSION['user_name']) && isset($_SESSION['consultant_name']))
             </div>
           </div>
           <div class="rbottom">
-            <div class="read-m">
-              <div class="read">
-                <div class="pic">
-                  <span class="material-symbols-outlined">
-                    sick
+            <div class="cases">
+              <a href="">
+              <div class="case">
+                <div class="name">
+                  <span class="material-symbols-rounded">
+                    healing
                   </span>
+                  Primary Care
                 </div>
-                <div class="topic">
-                  Disease-Specific Indications and Outcomes
-                </div>
-                <div class="desc">
-                  <br>Find the latest references, outcomes, and referral timing guidelines by disease. Access patient-friendly handouts.
-                  <br>
-                </div>
-                <div class="but1">
-                  <a href="consultant_disease.php">Browse All Diseases</a><br>
-                  <select id="d1" name="disease" onchange="selectDisease();">
-                    <option selected="selected" disabled=disabled>Select the Disease</option>
-                    <option value="AML-Adult">AML - Adult</option>
-                    <option value="AML-Pediatric">AML-Pediatric</option>
-                    <option value="ALL-Adult">ALL-Adult</option>
-                    <option value="ALL-Pediatric">ALL-Pediatric</option>
-                    <option value="MDS">MDS</option>
-                    <option value="CML">CML</option>
-                    <option value="CLL">CLL</option>
-                    <option value="NHL">NHL</option>
-                    <option value="HodgkinLymphoma">Hodgkin Lymphoma</option>
-                    <option value="MultipleMyeloma">Multiple Myeloma</option>
-                    <option value="SevereAplasticAnemia&MarrowFailure">Severe Aplastic Anemia & Marrow Failure</option>
-                    <option value="SickleCellDisease">Sickle Cell Disease</option>
-                    <option value="ImmuneDeficiencyDiseases">Immune Deficiency Diseases</option>
-                    <option value="InheritedMetabolicDisorders">Inherited Metabolic Disorders</option>
-                    <option value="Thalassemia">Thalassemia</option>
-                    <option value="OtherDiseases">Other Diseases</option>
-                    <option value="MultipleSclerosis">Multiple Sclerosis</option>
-                    <option value="SystemicSclerosis">Systemic Sclerosis</option>
-                  </select>
-                  <!--<select class="fancy_select fancy_selects-active" name="diseases" id="diseases" onchange="gotoDisease(this.value);" style="opacity: 0; min-height: 100%; min-width: 100%; position: absolute; top: 0px; left: 0px; z-index: 2; cursor: pointer;">-->
+                <div class="descript">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </div>
               </div>
-              <div class="read">
-                <div class="pic">
-                  <span class="material-symbols-outlined">
-                    rheumatology
+              </a>
+              <a href="">
+              <div class="case">
+                <div class="name">
+                  <span class="material-symbols-rounded">
+                    emergency
                   </span>
+                  Emergency Cases
                 </div>
-                <div class="topic">
-                  HLA Today
+                <div class="descript">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </div>
-                <div class="desc">
-                    Remove barriers to patient and family member HLA typing at diagnosis. Get the critical info you need to make treatment decisions.
+              </div>
+              </a>
+              <a href="">
+              <div class="case">
+                <div class="name">
+                  <span class="material-symbols-rounded">
+                    calendar_apps_script
+                  </span>
+                  Online Appoinments
                 </div>
-                <div class="but2">
-                  <a href="consultant_discover.php">Discover How</a>
+                <div class="descript">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </div>
+                </a>
+              </div>
+            </div>
+            <div class="banner2">
+              <div class="phara2">
+                <p>This is a phara. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <button type="button" name="button">Read More</button>
               </div>
             </div>
             <div class="banner">
