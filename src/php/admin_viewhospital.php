3b73fcaa3b987f7bdd1102657c704e6ebd5ede86
Samadhika001 <92731546+Samadhika001@users.noreply.github.com>
2022-11-21 14:59:42 +0530

    new commit

--- src/php/admin_viewhospital.php
+++ src/php/admin_viewhospital.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+include "../../config/connection.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,10 +10,56 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="../../public/css/admin_viewhospital.css">
 </head>
 <body>
-    <button>
-        Add Hospital
-    </button>
+<a href="admin_addhospital.php"><button class="create-btn"> + Add Hospital
+       </button></a>
+
+       <table class=" table">
+        <thead>
+        <tr>
+        <th class="table-head">
+      Hospital ID
+      </th>
+      <th class="table-head">
+      Hospital Name
+      </th><th class="table-head">
+      Hospital Email
+      </th><th class="table-head">
+      Hospital Telephone No.
+      </th><th class="table-head">
+      Hospital Address
+      </th>
+        </tr>
+        </thead>
+       <tbody>
+
+        <?php 
+        $sql="select * from `hospital`";
+        $result=mysqli_query($connection,$sql);
+        if($result){
+            while($row=mysqli_fetch_assoc($result)){
+                $id=$row['hospital_id'];
+                $name=$row['hospital_name'];
+                $email=$row['email'];
+                $tele=$row['telephone_no'];
+                $address=$row['address'];
+             echo '<tr>
+        <td>'.$id. '</td>
+        <td>'.$name. '</td>
+        <td>'.$email. '</td>
+        <td>'.$tele. '</td>
+        <td>'.$address. '</td>      
+    </tr>';
+
+            }
+        }
+        
+        ?>
+        
+       </tbody>
+      
+       </table>
 </body>
 </html>
