2ef5afbe620a3badf7c5033b7302960ea1c47d84
Rashi990 <rashinilakshika2020@gmail.com>
2022-11-24 10:40:36 +0530

    hospital login ,updated files

--- src/php/hospital_consultants.php
+++ src/php/hospital_consultants.php
@@ -1,8 +1,11 @@
-<?php require_once('hospital_navbar.php'); ?>
-<?php session_start();?>
-
-<?php require_once('../../config/connection.php');?>
-
+<?php
+require_once('../../config/connection.php');
+session_start();
+if (!isset($_SESSION['user_name']) && !isset($_SESSION['hospital_name']) )
+{
+    header("Location:hospital_login.php");
+}
+ ?>
 
 <?php include('../../public/html/hospital_consultants.html'); ?>
 <?php require_once('hospital_footer.php'); ?>
--- src/php/hospital_donors.php
+++ src/php/hospital_donors.php
@@ -1,8 +1,13 @@
+<?php
+require_once('../../config/connection.php');
+session_start();
+if (!isset($_SESSION['user_name']) && !isset($_SESSION['hospital_name']) )
+{
+    header("Location:hospital_login.php");
+}
+ ?>
+
 <?php require_once('hospital_navbar.php'); ?>
-<?php session_start();?>
-
-<?php require_once('../../config/connection.php');?>
-
 
 <?php include('../../public/html/hospital_donors.html'); ?>
 <?php require_once('hospital_footer.php'); ?>
