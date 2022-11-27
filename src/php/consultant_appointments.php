e57e8ec235d42f6e87993067d9f8d708a1039142
thilini.yashodha.demel <thilini.yashodha.demel@gmail.com>
2022-11-27 12:08:02 +0530

    Consultant Pages Modification

--- src/php/consultant_appointments.php
+++ src/php/consultant_appointments.php
@@ -9,9 +9,10 @@ if (isset($_SESSION['user_name']) && isset($_SESSION['consultant_name']))
   <head>
     <meta charset="utf-8">
     <title>Consultant Appointments</title>
-    <link rel="stylesheet" type="text/css" href="../../public/css/consultant_appointments.css">
+    <link rel="stylesheet" type="text/css" href="../../public/css/consultant_appointments.css?v=1">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   </head>
   <body>
     <div class="hero">
@@ -42,40 +43,50 @@ if (isset($_SESSION['user_name']) && isset($_SESSION['consultant_name']))
         <div class="right">
           <div class="rtop">
             <div class="empty">
-              <p>Appointments</p>
+              <p>Appoinments</p>
             </div>
             <div class="profile">
-              <abbr title="notifications"><a href="../../public/html/consultant_notifications.html">
-                <div class="icon">
-                  <span class="material-symbols-outlined" style="font-size:35px;">
-                    notifications
-                  </span>
-                </div>
-              </a></abbr>
-              <abbr title="messages"><a href="../../public/html/consultant_messages.html">
-                <div class="icon">
-                  <span class="material-symbols-outlined" style="font-size:35px;">
-                    message
-                  </span>
-                </div>
-              </a></abbr>
-              <abbr title="messages"><a href="consultant_home.php">
-                <div class="icon">
-                  <span class="material-symbols-outlined" style="font-size:35px;">
-                    home
-                  </span>
-                </div>
-              </a></abbr>
-                <abbr title="Welcome!"><div class="greet">
+              <abbr title="notifications">
+                <a href="../../public/html/consultant_notifications.html">
+                  <div class="icon">
+                    <span class="material-icons">
+                      notifications
+                    </span>
+                  </div>
+                </a>
+              </abbr>
+              <abbr title="messages">
+                <a href="../../public/html/consultant_messages.html">
+                  <div class="icon">
+                    <span class="material-icons">
+                      chat_bubble
+                    </span>
+                  </div>
+                </a>
+              </abbr>
+              <abbr title="Home">
+                <a href="consultant_home.php">
+                  <div class="icon">
+                    <span class="material-icons">
+                      home
+                    </span>
+                  </div>
+                </a>
+              </abbr>
+              <abbr title="Welcome!">
+                <div class="greet">
                   <h3 class="greet-text">Hi, Dr.<?php echo $_SESSION['consultant_name'];?></h3>
-                </div></abbr>
-              <abbr title="Profile"><a href="../../public/html/consultant_profile.html">
-                <div class="pp">
-                  <span class="material-symbols-outlined" style="font-size:35px;">
-                    account_circle
-                  </span>
                 </div>
-              </a></abbr>
+              </abbr>
+              <abbr title="Profile">
+                <a href="../../public/html/consultant_profile.html">
+                  <div class="pp">
+                    <span class="material-icons">
+                      account_circle
+                    </span>
+                  </div>
+                </a>
+              </abbr>
             </div>
           </div>
           <div class="rbottom">
