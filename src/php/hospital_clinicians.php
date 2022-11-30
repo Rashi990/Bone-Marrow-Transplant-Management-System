0c47b6c3f8881ed32aa7a4be529785e348f233db
Rashi990 <rashinilakshika2020@gmail.com>
2022-11-30 14:26:48 +0530

    completed hospital_add_clinicians, updated sql

--- src/php/hospital_clinicians.php
+++ src/php/hospital_clinicians.php
@@ -2,38 +2,13 @@
 <?php
 require_once('../../config/connection.php');
 session_start();
-if (!(isset($_SESSION['user_name']) && isset($_SESSION['hospital_id']) ))
+if (!(isset($_SESSION['user_name']) && isset($_SESSION['hospital_name']) ))
 {
     header("Location:hospital_login.php");
 }
+?>
 
-$clinician_list = '';
-
-$hospital_id = $_SESSION['hospital_id'];
-
-$query = "SELECT clinician_id, clinician_name, telephone_no, address FROM clinician WHERE hospital_id = $hospital_id ";
-$clinicians = mysqli_query($connection,$query);
-
-if($clinicians){
-    while($clinician = mysqli_fetch_assoc($clinicians)){
-        $clinician_list .= "<tr>";
-        $clinician_list .= "<td>{$clinician['clinician_id']}</td>";
-        $clinician_list .= "<td>{$clinician['clinician_name']}</td>";
-        $clinician_list .= "<td>{$clinician['telephone_no']}</td>";
-        $clinician_list .= "<td>{$clinician['address']}</td>";
-        $clinician_list .= "<td><a href=\"hospital_update_clinicians.php?clinician_id = {$clinician['clinician_id']}\">View</a></td>";
-        $clinician_list .= "<td><a href=\"hospital_update_clinicians.php?clinician_id = {$clinician['clinician_id']}\">Edit</a></td>";
-        $clinician_list .= "<td><a href=\"hospital_delete_clinicians.php?clinician_id = {$clinician['clinician_id']}\">Delete</a></td>";
-        $clinician_list .= "</tr>";
-    }
-}
-else{
-    echo "Database query failed!";
-}
-
- ?>
-
- <!DOCTYPE html>
+<!DOCTYPE html>
  <html lang="en">
  <head>
     <meta charset="UTF-8">
@@ -52,6 +27,7 @@ else{
       </div>
  </body>
  </html>
+
 
 <?php include('../../public/html/hospital_clinicians.html'); ?>
 <?php require_once('hospital_footer.php'); ?>
