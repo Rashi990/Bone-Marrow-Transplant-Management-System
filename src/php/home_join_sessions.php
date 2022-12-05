0a834eb139f466ecc0ec2573a28fa3df4efe0d74
Rashi990 <rashinilakshika2020@gmail.com>
2022-12-05 11:35:47 +0530

    hospital dashboard, index/home pages

---
+++ src/php/home_join_sessions.php
@@ -0,0 +1,7 @@
+<?php require_once('navigation.php'); ?>
+<?php session_start(); ?>
+
+<?php require_once('../../config/connection.php');?>
+
+<?php include('../../public/html/home_join_sessions.html'); ?>
+<?php require_once('footer.php'); ?>
