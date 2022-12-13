b1ddc3aa19f567122bc1228f98d96ff310475eaa
Subodhinie99 <subodhiniechathurajika@gmail.com>
2022-12-13 06:06:04 +0530

    update

--- src/php/donor_sign_validations.php
+++ src/php/donor_sign_validations.php
@@ -28,9 +28,9 @@ function usernameInvalid($user_name){
 
 //Check if register inputs are empty
 
-function inputsEmptyRegister($fname, $lname, $email, $mobile, $pass ,$re_pass ){
+function inputsEmptyRegister($fname, $lname, $email, $pass ,$re_pass){
     
-    if(empty($fname)||empty($lname)||empty($email)||empty($mobile)||empty($pass)||empty($re_pass)){
+    if(empty($fname)||empty($lname)||empty($email)||empty($pass)||empty($re_pass)){
         $value = true;
 
     }
@@ -43,16 +43,16 @@ function inputsEmptyRegister($fname, $lname, $email, $mobile, $pass ,$re_pass ){
 
 
 //Check if names are invalid
-function nameInvalid($fname, $lname){
+function nameInvalid($fname){
 
     if(!preg_match("/^[a-zA-Z]+$/",$fname)){ 
         $value = true;
 
     }
-    elseif(!preg_match("/^[a-zA-Z]+$/",$lname)){
+   /* elseif(!preg_match("/^[a-zA-Z]+$/",$lname)){
     
         $value = true;
-    }
+    }*/
     else{
         $value = false;
     }
@@ -74,7 +74,7 @@ function emailInvalid($email){
 
 
 //Check if mobile is invalid
-function mobileInvalid($mobile){
+/*function mobileInvalid($mobile){
    
     if(!preg_match("/^[0][\d]{9}$/",$mobile)){
         $value = true;
@@ -83,7 +83,7 @@ function mobileInvalid($mobile){
         $value = false;
     }
     return $value;
-}
+}*/
 
 //Check if password is invalid
 function passwordInvalid($pass){
@@ -111,20 +111,20 @@ function passNotMatch($pass, $re_pass){
 
 
 //Check if email or mobile available in the system
-function emailOrMobileAvailable($connection, $email, $mobile){
+function emailAvailable($connection, $email){
    
     //Query
-    $sql = "SELECT * FROM donor WHERE email =? OR telephone_no =?;";
+    $sql = "SELECT * FROM donor WHERE email =? ;";
     //Initialize the prepared statement 
     $stmt = mysqli_stmt_init($connection);
     //Bind the statement with the query and check errors
-    if(mysqli_stmt_prepare($stmt, $sql)){
+    if(!mysqli_stmt_prepare($stmt, $sql)){
         header("location: donor_signup_index.php?err=failedstmt");
         exit();
     } 
     else {
         //Bind data with the statement
-        mysqli_stmt_bind_param($stmt, "si", $email, $mobile);
+        mysqli_stmt_bind_param($stmt, "s", $email);
         //Execute the statement
         mysqli_stmt_execute($stmt);
         //Save results if available
