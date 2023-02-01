5fc494d7aba92125d2e8d79d316ac49f60e92a53
thilini.yashodha.demel <thilini.yashodha.demel@gmail.com>
2023-02-01 14:31:56 +0530

    Update Consultant and Clinician

--- src/php/consultant_calendar.php
+++ src/php/consultant_calendar.php
@@ -1,113 +1,34 @@
+<?php require_once('consultant_navbar_calendar.php'); ?>
 <?php
+require_once('../../config/connection.php');
 session_start();
-if (isset($_SESSION['user_name']) && isset($_SESSION['consultant_name']))
+if (!(isset($_SESSION['user_name']) && isset($_SESSION['consultant_name']) ))
 {
-?>
+    header("Location:consultant_login.php");
+}
+ ?>
 
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Consultant Calendar</title>
-    <link rel="stylesheet" type="text/css" href="../../public/css/consultant_calendar.css?v=1">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0" />
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../../public/css/consultant_navbar.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  </head>
-  <body>
-    <div class="hero">
-      <div class="top">
-        <div class="left">
-            <img src="../../public/images/logo.png" class="logo">
+    <title>Top</title>
+</head>
+<body>
 
-          <div class="tabs">
-
-            <a class="hi" href="consultant_appointments.php">Appointments</a>
-            <a class="hi" href="consultant_reports.php">Reports</a>
-            <a class="hi-selected">Calendar</a>
-            <a class="hi" href="consultant_patient.php">Patient</a>
-            <a class="hi" href="consultant_donor.php">Donor</a>
-
-            <div class="logout">
-              <abbr title="Logout">
-                <a class="hi" href="consultant_logout.php">
-                  <span class="material-symbols-rounded">
-                    logout
-                  </span>
-                </a>
-              </abbr>
-            </div>
-
-          </div>
-        </div>
-        <div class="right">
-          <div class="rtop">
-            <div class="empty">
-              <div class="search-bar">
-                <span class="material-icons">search</span>
-                <input type="search" placeholder="search here">
-              </div>
-            </div>
-            <div class="profile">
-              <abbr title="notifications">
-                <a href="../../public/html/consultant_notifications.html">
-                  <div class="icon">
-                    <span class="material-icons">
-                      notifications
-                    </span>
-                  </div>
-                </a>
-              </abbr>
-              <abbr title="messages">
-                <a href="../../public/html/consultant_messages.html">
-                  <div class="icon">
-                    <span class="material-icons">
-                      chat_bubble
-                    </span>
-                  </div>
-                </a>
-              </abbr>
-              <abbr title="Home">
-                <a href="consultant_home.php">
-                  <div class="icon">
-                    <span class="material-icons">
-                      home
-                    </span>
-                  </div>
-                </a>
-              </abbr>
-              <abbr title="Welcome!">
-                <div class="greet">
-                  <h3 class="greet-text">Welcome! Dr.<?php echo $_SESSION['consultant_name'];?></h3>
-                </div>
-              </abbr>
-              <abbr title="Profile">
-                <a href="../../public/html/consultant_profile.html">
-                  <div class="pp">
-                    <span class="material-icons">
-                      account_circle
-                    </span>
-                  </div>
-                </a>
-              </abbr>
-            </div>
-          </div>
-          <div class="rbottom">
-            <?php include('../../public/html/consultant_calendar.html'); ?>
-          </div>
-        </div>
+<div class="top">
+        <span class="material-icons">notifications</span>
+        <span class="material-icons">chat_bubble</span>
+        <div class="Loggedin"> Welcome! <?php echo $_SESSION['consultant_name'];?></div>
+        <span class="material-icons">account_circle</span>
       </div>
-      <div class="bottom">
-        <div class="bottom-input">
-          <span>© 2022 SLBMTMS. All rights reserved.</span>
-        </div>
-        <div class="bottom-input">
-          <span>Terms and conditions</span>
-        </div>
-      </div>
-    </div>
-  </body>
+
+</body>
 </html>
 
-<?php
-}
-?>
+<?php include('../../public/html/consultant_calendar.html'); ?>
+<!--<?php require_once('consultant_footer.php'); ?>-->
