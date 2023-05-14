62caeb47029ea78f823fd114e50ff124919aebfe
Rashi990 <rashinilakshika2020@gmail.com>
2023-05-14 12:52:22 +0530

    hospitalstock,testing requests,match requests

--- src/php/clinician_match.php
+++ src/php/clinician_match.php
@@ -1,11 +1,14 @@
-<?php require_once('clinician_navbar_match.php'); ?>
+<?php require_once('clinician_navbar.php'); ?>
 <?php
 require_once('../../config/connection.php');
 session_start();
-if (!(isset($_SESSION['user_name']) && isset($_SESSION['clinician_name']) ))
+if($_SESSION['userlevel']!=2)
 {
-    header("Location:clinician_login.php");
+    header("Location:login.php");
 }
+
+$uid=$_SESSION['uid'];
+$username=$_SESSION['username'];
  ?>
 
 <!DOCTYPE html>
@@ -32,16 +35,16 @@ if (!(isset($_SESSION['user_name']) && isset($_SESSION['clinician_name']) ))
         </div>
       </div>
 
-    <div class="top">
+      <div class="top">
       <span class="material-icons">notifications</span>
       <span class="material-icons">chat_bubble</span>
-      <div class="Loggedin"> Welcome! <?php echo $_SESSION['clinician_name'];?></div>
+      <div class="Loggedin"> Welcome! <?php echo $username;?></div>
       <span class="material-icons">account_circle</span>
     </div>
   </div>
-
+  
 </body>
 </html>
 
-<?php include('clinician_matching.php'); ?>
-<!--<?php require_once('consultant_footer.php'); ?>-->
+<?php include('../../public/html/clinician_match.html'); ?>
+
--- src/php/hospital_update_clinicians.php
+++ src/php/hospital_update_clinicians.php
@@ -8,11 +8,58 @@ if($_SESSION['userlevel']!=3)
 
 $uid=$_SESSION['uid'];
 $username=$_SESSION['username'];
+$hospital_id=$_SESSION['hospital_id'];
 
 if(isset($_GET['updateid'])){
     $id=$_GET['updateid'];
 
-   
+   $sql1="SELECT * FROM clinician WHERE hospital_id=$hospital_id";
+   $result1=mysqli_query($connection,$sql1);
+   if($result1){
+    while($row=mysqli_fetch_assoc($result1)){
+        $cID = $row['clinician_id'];
+        $clinician_name = $row['clinician_name'];
+        $c_email = $row['email'];
+        $tele = $row['telephone_no'];
+        $address = $row['address'];
+        $c_username = $row['username'];
+        //$c_password = $row['password'];
+		//$conpassword=$row['conpassword'];
+    }
+   }
+
+   $sql2="SELECT * FROM account WHERE uid=$uid";
+   $result2=mysqli_query($connection,$sql2);
+   if($result2){
+    while($row=mysqli_fetch_assoc($result2)){
+        $c_username = $row['username'];
+        //$conpassword = $row['password'];
+   }
+}
+
+   if(isset($_POST['submit'])){
+        $clinician_name = $_POST['clinician_name'];
+        $c_email = $_POST['email'];
+        $tele = $_POST['telephone_no'];
+        $address = $_POST['address'];
+        $c_username = $_POST['username'];
+        //$c_password = $_POST['password'];
+        //$conpassword=$_POST['conpassword']; 
+
+        $sql3="UPDATE clinician SET clinician_id=$cID, clinician_name=$clinician_name, email=$c_email, telephone_no=$tele, address=$address, username=$c_username";
+        $result3=mysqli_query($connection,$sql3);
+        if($result3){
+            echo "<script> alert('Sucessfully updated') </script>";
+			header("Location: hospital_clinicians.php");
+
+            //$sql4="UPDATE account SET uid=$uid, username=$c_username";
+            //$result4=mysqli_fetch_assoc($connection,$sql4);
+            
+        }
+
+        echo "<script> alert('Failed') </script>";
+   }
+
 }
 
  ?>
@@ -32,5 +79,5 @@ if(isset($_GET['updateid'])){
 
 
 
-?>
+
 <?php include('../../public/html/hospital_update_clinicians.html'); ?>
