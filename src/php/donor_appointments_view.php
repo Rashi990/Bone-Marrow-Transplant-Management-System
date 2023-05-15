19c04999e4af756610bea3c51a796b805a04ab63
Subodhinie99 <subodhiniechathurajika@gmail.com>
2023-05-15 14:45:34 +0530

    Update_donor_appointments_view

--- src/php/donor_appointments_view.php
+++ src/php/donor_appointments_view.php
@@ -17,7 +17,7 @@ require_once('../../config/connection.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../../public/css/donor_appointments.css"> 
+    <link rel="stylesheet" type="text/css" href="../../public/css/donor_appointments_view.css"> 
     <title>SLBTMS</title>
 </head>
 <body>
