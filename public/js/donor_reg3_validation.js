b28b021887f8ede0cd09dceb44455772cb5c16f0
Subodhinie99 <subodhiniechathurajika@gmail.com>
2023-05-16 09:31:24 +0530

    update_reg

--- public/js/donor_reg3_validation.js
+++ public/js/donor_reg3_validation.js
@@ -1,12 +1,15 @@
 const form = document.getElementById('form');
-const firstname = document.getElementById('fname');
-const lastname = document.getElementById('lname');
+const fullname = document.getElementById('fullname');
+//const lastname = document.getElementById('lname');
 const nic = document.getElementById('nic');
 const dob = document.getElementById('dob');
 const email = document.getElementById('email');
 const tel = document.getElementById('tel');
+const city=document.getElementById('city');
 const address = document.getElementById('address');
-
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const conf_password = document.getElementById('conf_password');
 
 
 
@@ -63,30 +66,50 @@ $(document).ready(function() {
 
 //const validateInputs = () => {
     function validateInputs () {
-    const firstnameValue = firstname.value.trim();
-    const lastnameValue = lastname.value.trim();
+    const fullnameValue = fullname.value.trim();
+   // const lastnameValue = lastname.value.trim();
     const nicValue = nic.value.trim();
    const emailValue = email.value.trim();
    const telValue = tel.value.trim();
+   const cityValue=city.value.trim();
    const addressValue = address.value.trim();
-   const cityValue = city.value.trim();
+   const usernameValue = username.value.trim();
+   const passwordValue = password.value.trim();
+   const conf_passwordValue = conf_password.value.trim();
 
-    if(firstnameValue === '') {
-        setError(firstname, 'First Name is required');
-    } else if (!isValid_FirstName(firstnameValue)){
-        setError(firstname, 'Provide a valid Name');
+
+    if(fullnameValue === '') {
+        setError(fullname, 'Full Name is required');
+    } else if (!isValid_FullName(fullnameValue)){
+        setError(fullname, 'Provide a valid Name');
     } else{
-        setSuccess(firstname);
+        setSuccess(fullname);
     }
 
-    if(lastnameValue === '') {
+  /*  if(lastnameValue === '') {
         setError(lastname, 'Last Name is required');
     } else if (!isValid_LastName(lastnameValue)){
         setError(lastname, 'Provide a valid Name');
     } else {
         setSuccess(lastname);
     }
-    
+  */  
+
+    if(passwordValue===''){
+        setError(nic, 'Password is required');
+    } 
+    else if (!isValidPassword(passwordValue)){
+        setError(password, 'Provide a password more than 5 characters');
+    } else{
+        setSuccess(password);
+    }
+
+
+
+
+
+
+
 
     if(nicValue===''){
         setError(nic, 'National Identity Card Number is required');
@@ -120,29 +143,61 @@ $(document).ready(function() {
 
     if(cityValue === '') {
         setError(city, 'City Name is required');
+       
     } else{
         setSuccess(city);
     }
+
+
+    if(usernameValue === '') {
+        setError(username, 'Username is required');
+    } else{
+        setSuccess(username);
+    }
+
+    if(passwordValue === '') {
+        setError(password, 'Password is required');
+    } else{
+        setSuccess(password);
+    }
+
+    if(conf_passwordValue === '') {
+        setError(conf_password, 'Confirm Password is required');
+    } else{
+        setSuccess(conf_password);
+    }
+
+
+    
+    if(!(passwordValue === conf_passwordValue)) {
+        setError(conf_password, 'Confirm Password must be equal to  required');
+    } else{
+        setSuccess(conf_password);
+    }
+
 
 
 
 };
 
 
-
-
-
-
-const isValid_FirstName= firstname=>{
-    const re = /^[a-zA-Z]+$/;
-    return re.test(firstname);
+const isValidPassword= password=>{
+    const re=/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/;
+    return re.test(password);
 }
 
-const isValid_LastName= lastname=>{
+
+
+const isValid_FullName= fullname=>{
+    const re =/^[a-zA-Z]+ [a-zA-Z]+(?: [a-zA-Z]+)*$/;
+    return re.test(fullname);
+}
+
+/*const isValid_LastName= lastname=>{
     const re = /^[a-zA-Z]+$/;
     return re.test(lastname);
 }
-
+*/
 const isValidNic = nic =>{
    const re= /^([0-9]{9}[x|X|v|V]|[0-9]{12})$/;
     //const re=/^((19[5-9][3-9]|[2-9][0-9]{3})\d{8})|([8-9]{2}\d{7}[vV])$/;
@@ -209,7 +264,7 @@ if (tdate < 10) {
     tdate = "0" + tdate;
 }
 var maxDate = year + "-" + month + "-" + tdate;
-var lastYear = todayDate.getUTCFullYear()-70;
+var lastYear = todayDate.getUTCFullYear()-40;
 var minDate = lastYear + "-" + month + "-" + tdate;
 document.getElementById("dob").setAttribute("max", maxDate);
 document.getElementById("dob").setAttribute("min", minDate);
--- src/php/donor_reg3.php
+++ src/php/donor_reg3.php
@@ -13,8 +13,8 @@ require_once "../../src/php/donor_reg_validations.php";
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //Get form input data
    
-    $fname = trim($_POST["fname"]);
-    $lname =trim($_POST["lname"]);
+    $fullname = trim($_POST["fullname"]);
+   // $lname =trim($_POST["lname"]);
     $email = trim($_POST["email"]);
     $tel = trim($_POST["tel"]);
     $address = trim($_POST["address"]);
@@ -22,16 +22,20 @@ require_once "../../src/php/donor_reg_validations.php";
     $district = trim($_POST["district"]);
     $dob = trim($_POST["dob"]);
     $nic = trim($_POST["nic"]);
+    $username = trim($_POST["username"]);
+    $password = trim($_POST["password"]);
+    $conf_password = trim($_POST["nic"]);
     $marital = trim($_POST["marital"]);
     $gender = trim($_POST["gender"]);
+    $blood_group=trim($_POST["blood_group"]);
    
     
 
     //Input validation
     
-    if (inputsEmptyRegister($fname,$lname,$email,$tel,$address,$city,$district,$dob,$nic,$marital,$gender)) {
+    if (inputsEmptyRegister($fullname,$email,$tel,$address,$city,$district,$dob,$nic,$marital,$gender,$username,$password,$conf_password, $blood_group)) {
         header("location:donor_reg3_index.php?error=Please fill all the fields");
-    } else if (nameInvalid($fname,$lname)) {
+    } else if (nameInvalid($fullname)) {
         header("location: donor_signup_index.php?error=Names are Invalid");
     } 
     
@@ -54,7 +58,7 @@ require_once "../../src/php/donor_reg_validations.php";
     
     else {
         //If all inputs are error free
-    registerNewUser($connection, $fname,$lname,$email,$tel,$address,$city,$district,$dob,$gender,$nic,$marital);
+    registerNewUser($connection, $fullname,$email,$tel,$address,$city,$district,$dob,$nic,$marital,$gender,$username,$password,$conf_password, $blood_group);
       
 
 
@@ -68,30 +72,76 @@ else{
 }
 
 
-//Function for register a new user
-function registerNewUser($connection, $fname,$lname,$email,$tel,$address,$city,$district,$dob,$gender,$nic,$marital){
-    //Query
-    $sql = "INSERT INTO pending_donor (first_name,second_name,email,telephone_no,address,city,district,date_of_birth,gender,id_number,marital_state) VALUES (?,?,?,?,?,?,?,?,?,?,?); ";
-       //Initialize the prepared statement 
-       $stmt = mysqli_stmt_init($connection);
-       //Bind the statement with the query and check errors
-       if(!mysqli_stmt_prepare($stmt, $sql)){
-           header("location: donor_reg3_index.php?error=failedstmt");
+// //Function for register a new user
+// function registerNewUser($connection, $fullname,$email,$tel,$address,$city,$district,$dob,$gender,$nic,$marital,$blood_group,$username,$password,$conf_password){
+//     //Query
+//     $sql = "INSERT INTO donor (donor_name,email,telephone_no,address,city,district,date_of_birth,gender,id_number,marital_state,blood_group,user_name,password,conf_password) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?); ";
+//        //Initialize the prepared statement 
+//        $stmt = mysqli_stmt_init($connection);
+//        //Bind the statement with the query and check errors
+//        if(!mysqli_stmt_prepare($stmt, $sql)){
+//            header("location: donor_reg3_index.php?err=failedstmt");
        
-       } 
-       else {
-           //Bind data with the statement
-           mysqli_stmt_bind_param($stmt, "sssdsssssds", $fname,$lname,$email,$tel,$address,$city,$district,$dob,$gender,$nic,$marital);
-           //Execute the statement
-           mysqli_stmt_execute($stmt);
-           //close the statement
-        mysqli_stmt_close($stmt);
-        header("location: donor_reg3_index.php?error=successfully Added");
+//        } 
+//        else {
+//            //Bind data with the statement
+//            mysqli_stmt_bind_param($stmt, "ssssssssssss", $fullname, $email, $tel, $address, $city, $district, $dob, $gender, $nic, $marital, $blood_group, $username, $password, $conf_password);
+           
+//            //Execute the statement
+//            mysqli_stmt_execute($stmt);
+//            //close the statement
+//         mysqli_stmt_close($stmt);
+//         header("location: donor_reg3_index.php?err=successfully Added");
         
+//     }
+
+
+// } 
+
+// 
+
+
+//Function for registering a new user
+function registerNewUser($connection, $fullname, $email, $tel, $address, $city, $district, $dob, $gender, $nic, $marital, $blood_group, $username, $password, $conf_password){
+    // Escape the variables to prevent SQL injection (optional but recommended)
+    $fullname = mysqli_real_escape_string($connection, $fullname);
+    $email = mysqli_real_escape_string($connection, $email);
+    $tel = mysqli_real_escape_string($connection, $tel);
+    $address = mysqli_real_escape_string($connection, $address);
+    $city = mysqli_real_escape_string($connection, $city);
+    $district = mysqli_real_escape_string($connection, $district);
+    $dob = mysqli_real_escape_string($connection, $dob);
+    $gender = mysqli_real_escape_string($connection, $gender);
+    $nic = mysqli_real_escape_string($connection, $nic);
+    $marital = mysqli_real_escape_string($connection, $marital);
+    $blood_group = mysqli_real_escape_string($connection, $blood_group);
+    $username = mysqli_real_escape_string($connection, $username);
+    $password = mysqli_real_escape_string($connection, $password);
+    $conf_password = mysqli_real_escape_string($connection, $conf_password);
+
+    //Query with concatenated values
+    $sql = "INSERT INTO donor (donor_name,email,telephone_no,address,city,district,date_of_birth,gender,id_number,marital_state,blood_group,user_name,password,conf_password) VALUES ('$fullname', '$email', '$tel', '$address', '$city', '$district', '$dob', '$gender', '$nic', '$marital', '$blood_group', '$username', '$password', '$conf_password')";
+
+    //Execute the query and check for errors
+    if (mysqli_query($connection, $sql)) {
+        header("location: donor_reg3_index.php?err=successfully Added");
+    } else {
+        header("location: donor_reg3_index.php?err=failedstmt");
     }
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
