c968861f90de516ddeade76dfb908c23c87ca9a2
Rashi990 <rashinilakshika2020@gmail.com>
2023-09-10 02:08:29 +0530

    matching fixed

--- src/php/hospital_patients_table.php
+++ src/php/hospital_patients_table.php
@@ -2,7 +2,7 @@
 
  $hospital_id=$_SESSION['hospital_id'];
 
- $sql="SELECT patient_id,patient_name,age,blood_group,diagnosis,telephone_no,address,level FROM patient WHERE hospital_id=$hospital_id ORDER BY patient_id ";
+ $sql="SELECT patient_id,patient_name,age,blood_group,diagnosis,telephone_no,address FROM patient WHERE hospital_id=$hospital_id ORDER BY patient_id ";
  //echo $sql;
 
  $result=mysqli_query($connection,$sql);
@@ -16,7 +16,6 @@
         $diagnosis=$row['diagnosis'];
         $tele=$row['telephone_no'];
         $addr=$row['address'];
-        $level=$row['level'];
 
         echo "<tr>";
 
@@ -27,9 +26,9 @@
             echo "<td>".$diagnosis."</td>";
             echo "<td>".$tele."</td>";
             echo "<td>".$addr."</td>";
-
-            //echo "<td><a href='hospital_view_patient.php?viewid=".$id."' class='view'><abbr title='View'><span class='material-icons'>visibility</span></abbr></a></td>";
-            //echo "<td><a href='hospital_update_patient.php?updateid=".$id."' class='edit'><abbr title='Edit'><span class='material-icons'>edit_square</span></abbr></a></td>";
+            
+            echo "<td><a href='hospital_view_patient.php?viewid=".$id."' class='view'><abbr title='View'><span class='material-icons'>visibility</span></abbr></a></td>";
+            echo "<td><a href='hospital_update_patient.php?updateid=".$id."' class='edit'><abbr title='Edit'><span class='material-icons'>edit_square</span></abbr></a></td>";
             echo "<td><a href='hospital_delete_patient.php?deleteid=".$id."' class='delete'><abbr title='Delete'><span class='material-icons'>delete</span></abbr></a></td>";
 
         echo "</tr>";
